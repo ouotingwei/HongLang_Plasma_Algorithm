f4f303101bd02d26561f1a10224e5dd533d5c425
tingweiouo <kklb1716@gmail.com>
2023-01-07 19:14:25 +0800

    normal estimate

--- path_planning.py
+++ path_planning.py
@@ -13,63 +13,37 @@ from matplotlib import pyplot as plt
 import open3d as o3d
 import csv
 
-pcd = o3d.io.read_point_cloud("001_rand.xyz")
+# read .xyz file
+pcd = o3d.io.read_point_cloud("test.xyz")
 
-o3d.visualization.draw_geometries([pcd])  
-print("原始點雲:",pcd)
+o3d.visualization.draw_geometries([pcd], window_name="test",
+                                  point_show_normal=True,
+                                  width=800, 
+                                  height=600)  
+print("origin : ",pcd)
 
-# downpcd = pcd.voxel_down_sample(voxel_size=4.5)
-downpcd = o3d.geometry.voxel_down_sample(pcd ,voxel_size=45)
+downpcd = pcd.voxel_down_sample(voxel_size=10)
 o3d.visualization.draw_geometries([downpcd])
 print('downsample pointcloud',downpcd)
 o3d.io.write_point_cloud('result_down.ply', downpcd)
 pcd = o3d.io.read_point_cloud("result_down.ply")
-#print(pcd)
 
-#print("->正在估计法线并可视化...")
-radius = 8   # 搜索半径
+radius = 8
 max_nn = 5
-o3d.geometry.estimate_normals(pcd,search_param=o3d.geometry.KDTreeSearchParamKNN(20))
-# pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius, max_nn))     # 执行法线估计
-# pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(max_nn) )   # 执行法线估计
 
+pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius, max_nn))
 
-### #有三種方式
-# o3d.geometry.KDTreeSearchParamKNN(knn=20)                        # 计算近邻的20个点
-# o3d.geometry.KDTreeSearchParamRadius(radius=0.01)                # 计算指定半径内的点
-# o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=20)
-pcd.paint_uniform_color([1, 0.706, 0])#source 为黄色
-# pcd[100].paint_uniform_color([0, 0.651, 0.929])#target 为蓝色
-
-# np.asarray(pcd.colors)[3, :] = [0, 0, 1]   #single point  painting
 totol_point = np.asarray(pcd.normals)[:,:].size/3
+
+pcd.paint_uniform_color([1, 0.706, 0])
 
 for i in range(0,int(totol_point)):
     if abs(np.asarray(pcd.normals)[i][1])  > 0.3:
         np.asarray(pcd.colors)[i, :] = [0, 0, 1]
 
-
-o3d.geometry.orient_normals_towards_camera_location(pcd, camera_location=np.array([20., 20., 100.]))  #朝向某個方向
-
-o3d.visualization.draw_geometries([pcd], window_name="result")
-
-
-print("列印出 法向量")
-print(np.asarray(pcd.normals)[:,:])
-
-# print(type(np.asarray(pcd.normals)))   ##<class 'numpy.ndarray'>
-print(np.asarray(pcd.normals)[1000][0])
-print(np.asarray(pcd.normals)[1000])
-
-
-
-print(type(pcd))
-print(np.asarray(pcd.normals)[:,:].size)
-
-
-print(type(pcd.normals))
-
-
-
-
+o3d.geometry.PointCloud.orient_normals_towards_camera_location(pcd, camera_location=np.array([0.0, 0.0, 1000.0])) 
+o3d.visualization.draw_geometries([pcd], window_name="result",
+                                  point_show_normal=True,
+                                  width=800,  
+                                  height=600)  
 
