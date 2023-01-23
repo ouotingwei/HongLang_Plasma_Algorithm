725e17f0539d66ec7814d3b13518137569435201
tingweiouo <kklb1716@gmail.com>
2023-01-23 22:06:36 +0800

    v2 structural optimization

--- plasma.py
+++ plasma.py
@@ -18,15 +18,6 @@ import math
 import  time
 
 
-global FileName
-global OutputFile
-global waypoints
-global PointArray
-global NormalArray
-waypoints = []
-breakPoint = 30 # > breakPoint high wall ; < breakPoint low wall
-
-
 class WayPoints:
     def __init__(self, x=0.000, y=0.000, z=0.000, W=0.000, P=0.000, R=0.000, V=100, C="CNT0"):
         self.x = x  # x
@@ -73,86 +64,49 @@ def pointCloudProcess(diameter, overlap):
     o3d.visualization.draw_geometries([pcd], window_name="result", point_show_normal=True)  
 
 
-def sampleFromOriginPCD(diameter, overlap):
-    # find the distance between two working path
-    sample_dis = diameter * (1 - (overlap*0.01))
-
-    # read .xyz file
-    global pcd 
-    pcd = o3d.io.read_point_cloud(FileName)
-
-    #o3d.visualization.draw_geometries([pcd], window_name="test", point_show_normal=True)  
-    #print("origin : ",pcd)
-
-    downpcd = pcd.voxel_down_sample(voxel_size=sample_dis)
-    #o3d.visualization.draw_geometries([downpcd])
-    print('downsample pointcloud',downpcd)
-    o3d.io.write_point_cloud('result_down.ply', downpcd)
-    pcd = o3d.io.read_point_cloud("result_down.ply")
-
-    #modular design not yet
-    radius = 50
-    max_nn = 50
-
-    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius, max_nn))
-    totol_point = np.asarray(pcd.normals)[:,:].size/3
-    pcd.paint_uniform_color([1, 0.706, 0])
-
-    for i in range(0,int(totol_point)):
-        if abs(np.asarray(pcd.normals)[i][1])  > 0.3:
-            np.asarray(pcd.colors)[i, :] = [0, 0, 1]
-
-    #modular design not yet
-    o3d.geometry.PointCloud.orient_normals_towards_camera_location(pcd, camera_location=np.array([0.0, 0.0, 10.])) 
-    o3d.visualization.draw_geometries([pcd], window_name="result", point_show_normal=True) 
-
-
-def lowWallPlanning():
-    PointArray = np.asarray(pcd.points)
-    NormalArray = np.asarray(pcd.normals)
-    
+def backAndForth(Point, Normal):
     #ARRANGE IN ORDER X
-    n = len(PointArray)
+    n = len(Point)
     for i in range(n-1):
         for j in range(n-i-1):
-            if PointArray[j][0] > PointArray[j+1][0]:
-                PointArray[j][0], PointArray[j+1][0] = PointArray[j+1][0], PointArray[j][0]
-                PointArray[j][1], PointArray[j+1][1] = PointArray[j+1][1], PointArray[j][1]
-                PointArray[j][2], PointArray[j+1][2] = PointArray[j+1][2], PointArray[j][2]
-                NormalArray[j][0], NormalArray[j+1][0] = NormalArray[j+1][0], NormalArray[j][0]
-                NormalArray[j][1], NormalArray[j+1][1] = NormalArray[j+1][1], NormalArray[j][1]
-                NormalArray[j][2], NormalArray[j+1][2] = NormalArray[j+1][2], NormalArray[j][2]
+            if Point[j][0] > Point[j+1][0]:
+                Point[j][0], Point[j+1][0] = Point[j+1][0], Point[j][0]
+                Point[j][1], Point[j+1][1] = Point[j+1][1], Point[j][1]
+                Point[j][2], Point[j+1][2] = Point[j+1][2], Point[j][2]
+                Normal[j][0], Normal[j+1][0] = Normal[j+1][0], Normal[j][0]
+                Normal[j][1], Normal[j+1][1] = Normal[j+1][1], Normal[j][1]
+                Normal[j][2], Normal[j+1][2] = Normal[j+1][2], Normal[j][2]
     
     #print(point_arr)
     #print("")
 
     #INITALIZATION
-    CountingArray = np.zeros(((len(PointArray) + 1, 8)), float)
+    CountingArray = np.zeros(((len(Point) + 1, 8)), float)
     i = 0
     flag = 0
     time = 1
     CountingArray[0][3] = 1
-    CountingArray[len(PointArray)-1][0] = PointArray[len(PointArray)-1][0]
-    CountingArray[len(PointArray)-1][1] = PointArray[len(PointArray)-1][1]
-    CountingArray[len(PointArray)-1][2] = PointArray[len(PointArray)-1][2]
+    CountingArray[len(Point)-1][0] = Point[len(Point)-1][0]
+    CountingArray[len(Point)-1][1] = Point[len(Point)-1][1]
+    CountingArray[len(Point)-1][2] = Point[len(Point)-1][2]
 
-    CountingArray[len(PointArray)-1][5] = NormalArray[len(PointArray)-1][0]
-    CountingArray[len(PointArray)-1][6] = NormalArray[len(PointArray)-1][1]
-    CountingArray[len(PointArray)-1][7] = NormalArray[len(PointArray)-1][2]
+    CountingArray[len(Point)-1][5] = Normal[len(Point)-1][0]
+    CountingArray[len(Point)-1][6] = Normal[len(Point)-1][1]
+    CountingArray[len(Point)-1][7] = Normal[len(Point)-1][2]
 
-    CountingArray[len(PointArray)][3] = 1
-    CountingArray[len(PointArray)][4] = 4
+    CountingArray[len(Point)][3] = 1
+    CountingArray[len(Point)][4] = 4
 
     #SORT
-    while i < len(PointArray) - 1:
-        CountingArray[i][0] = PointArray[i][0]
-        CountingArray[i][1] = PointArray[i][1]
-        CountingArray[i][2] = PointArray[i][2]
-        CountingArray[i][5] = NormalArray[i][0]
-        CountingArray[i][6] = NormalArray[i][1]
-        CountingArray[i][7] = NormalArray[i][2]
+    while i < len(Point) - 1:
+        CountingArray[i][0] = Point[i][0]
+        CountingArray[i][1] = Point[i][1]
+        CountingArray[i][2] = Point[i][2]
+        CountingArray[i][5] = Normal[i][0]
+        CountingArray[i][6] = Normal[i][1]
+        CountingArray[i][7] = Normal[i][2]
         
-        if round(PointArray[i+1][0]) != round(PointArray[i][0]) :
+        if round(Point[i+1][0]) != round(Point[i][0]) :
             flag = flag + 1
             CountingArray[i+1][4] = flag
             CountingArray[i+1][3] = time = 1
@@ -165,7 +119,7 @@ def lowWallPlanning():
 
     #ARRANGE IN ORDER Y
     i = 0
-    while i < len(PointArray) :
+    while i < len(Point) :
 
         if CountingArray[i+1][3] == 1 :
             n = int(CountingArray[i][3])
@@ -239,29 +193,37 @@ def lowWallPlanning():
         i = i + 1
     
     i = 0
-    while i < len(PointArray):
-        PointArray[i][0] = CountingArray[i][0]
-        PointArray[i][1] = CountingArray[i][1] 
-        PointArray[i][2] = CountingArray[i][2]
-        NormalArray[i][0] = CountingArray[i][5] = 90
-        NormalArray[i][1] = CountingArray[i][6] = 0
-        NormalArray[i][2] = CountingArray[i][7] = 90
+    while i < len(Point):
+        Point[i][0] = CountingArray[i][0]
+        Point[i][1] = CountingArray[i][1] 
+        Point[i][2] = CountingArray[i][2]
+        Normal[i][0] = CountingArray[i][5] = 90
+        Normal[i][1] = CountingArray[i][6] = 0
+        Normal[i][2] = CountingArray[i][7] = 90
 
         i = i + 1
-    
-    # new 
-    for i in range(0,len(PointArray)):
-        theta = 0
-        transition = [500 , -500, -270] 
+
+    global waypoints
+    waypoints = []
+
+    for i in range(0,len(Point)):
+        theta = 60*3.1415/180
+        transition = [280 , -400, -270] 
         rotation = np.array([[math.cos(theta), -math.sin(theta), 0], [math.sin(theta), math.cos(theta), 0], [0,0,1]], float)
-        position = np.matmul(PointArray[i], rotation) 
+        position = np.matmul(Point[i], rotation) 
         position = position + transition
-        waypoints.append(WayPoints(position[0], position[1], position[2], NormalArray[i][0], NormalArray[i][1], NormalArray[i][2]))
- 
+        waypoints.append(WayPoints(position[0], position[1], position[2], Normal[i][0], Normal[i][1], Normal[i][2]))
+
+
+def lowWallPlanning():
+    PointArray = np.asarray(pcd.points)
+    NormalArray = np.asarray(pcd.normals)
+    
+    backAndForth(PointArray, NormalArray)
+
 
 # !
 def highWallPlanning_Wall():
-
     return 0
 
 # !
@@ -269,40 +231,36 @@ def highWallPlanning_Bottom_Flat():
     PointArray = np.asarray(pcd.points)
     NormalArray = np.asarray(pcd.normals)
     
-    
-    
     i = 0
     size = 0
     while i < len(PointArray):
-        if int(PointArray[i][2]) == 0 :
+        if int(PointArray[i][2]) < 10 :
             size  = size + 1
         
         i = i + 1
     
-    CountingArray = np.zeros(((len(size) + 1, 8)), float)
+    Point_filter = np.zeros(((size + 1, 3)), float)
+    Normal_filter = np.zeros(((size + 1, 3)), float)
+
     i = 0
     cnt = 0
     while i < len(PointArray):
-        if int(PointArray[i][2]) == 0:
+        if int(PointArray[i][2]) < 10:
+            Point_filter[cnt][0] = PointArray[i][0]
+            Point_filter[cnt][1] = PointArray[i][1]
+            Point_filter[cnt][2] = PointArray[i][2]
+            Normal_filter[cnt][0] = NormalArray[i][0]
+            Normal_filter[cnt][1] = NormalArray[i][1]
+            Normal_filter[cnt][2] = NormalArray[i][2]
+
+            cnt = cnt + 1
             
 
         i = i + 1
 
-
-
+    backAndForth(Point_filter, Normal_filter)
 
     return 0
-
-
-#new
-def movePose():
-    for i in range(0,len(PointArray)):
-        theta = 60*3.1415/180
-        transition = [280 , -400, -270] 
-        rotation = np.array([[math.cos(theta), -math.sin(theta), 0], [math.sin(theta), math.cos(theta), 0], [0,0,1]], float)
-        position = np.matmul(PointArray[i], rotation) 
-        position = position + transition
-        waypoints.append(WayPoints(position[0], position[1], position[2], NormalArray[i][0], NormalArray[i][1], NormalArray[i][2]))
 
 
 def writeLsFile(file, waypoints):
@@ -361,6 +319,10 @@ def findMaxZ():
 
     
 def main():
+    global FileName
+    global OutputFile
+    breakPoint = 30 # > breakPoint high wall ; < breakPoint low wall
+
     diameter = float(input("[Q]diameter (mm) : "))
     overlap = int(input("[Q]overlap (0~90%) : "))
     FileName = str(input("[Q]file name(.xyz) : "))
@@ -371,9 +333,9 @@ def main():
     
     if findMaxZ() > breakPoint:
         # !
-        OutputFile = "WALL.LS"
-        highWallPlanning_Wall()
-        writeLsFile(OutputFile, waypoints)
+        #OutputFile = "WALL.LS"
+        #highWallPlanning_Wall()
+        #writeLsFile(OutputFile, waypoints)
 
         OutputFile = "BOTTOM.LS"
         highWallPlanning_Bottom_Flat()
@@ -381,9 +343,8 @@ def main():
         
 
     else:
-        OutputFile = "NORMAL.LS"
+        OutputFile = "LOWWALL.LS"
         lowWallPlanning()
-        movePose()
         writeLsFile(OutputFile, waypoints)
 
     end = time.time()
